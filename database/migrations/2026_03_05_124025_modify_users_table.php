6af2300d227d80f22b48f5234494c594103d09aa
Laurea Gregori <gregoridelrio@gmail.com>
2026-03-05 19:42:25 +0100

    add: migrations

---
+++ database/migrations/2026_03_05_124025_modify_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('phone', 9)->nullable();
+            $table->string('skill_level')->nullable();
+            $table->string('favourite_position')->nullable();
+            $table->string('role')->default('player');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['phone', 'skill_level', 'favourite_position', 'role']);
+        });
+    }
+};
