b749367a08baeec100c674911723766c3f2c477b
Laurea Gregori <gregoridelrio@gmail.com>
2026-03-19 10:36:25 +0100

    docs: swagger done in authcontroller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use OpenApi\Attributes as OA;
+
+#[OA\Info(title: "Meetup Football API", version: "1.0.0")]
+#[OA\SecurityScheme(
+    securityScheme: "bearerAuth",
+    type: "http",
+    description: "Enter JWT Bearer token",
+    scheme: "bearer",
+    bearerFormat: "JWT"
+)]
+#[OA\Server(url: "http://localhost:8000", description: "Local development server")]
 abstract class Controller
 {
     //
